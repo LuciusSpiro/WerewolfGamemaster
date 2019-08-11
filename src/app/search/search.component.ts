8c4d00e1137a2bdbb26b948cba7b1e0511de40ab
Jonas Noglik <noglik@rhrk.uni-kl.de>
2019-08-11 14:29:00 +0200

    Erste spielansicht und neue gitignore

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit } from "@angular/core";
+import { Player } from "./player";
+
 
 @Component({
     selector: "Search",
     moduleId: module.id,
     templateUrl: "./search.component.html"
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnInit{
+    public karten: Array<Player>;
     constructor() {
-        // Use the constructor to inject services.
     }
 
-    ngOnInit(): void {
-        // Use the "ngOnInit" handler to initialize data for the view.
+    ngOnInit() {
+        this.karten = [{name: "Dorfbewohner", text: "bla",  position: 1, gut: true, path: "nix"}, {name: "Werwolf", text: "bla",  position: 5, gut: false, path: "nix"}];
+        this.karten.sort((a1, a2) => a1.position - a2.position);
+    }
+
+    next() {
+        let first = this.karten.shift();
+        this.karten.push(first);
+        console.log(this.karten);
     }
 }
